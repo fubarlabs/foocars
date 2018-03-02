5ddfe1b020c37a4eecc75d12da59bb789971a0f0
digsrus <digsrus@gmail.com>
2018-03-02 10:51:08 -0500

    scp test

--- services/testSCP.py
+++ services/testSCP.py
@@ -7,11 +7,14 @@ import subprocess
 from subprocess import call
 
 #remotePath = 'jim@jims-Mac-mini.local:/home/jim/autonomous/'
-remotePath = 'jim@jims-XPS-13-9360.local:/home/jim/autonomous/'
+remotePath = 'jim@jim-XPS-13-9360.local:/home/jim/autonomous/'
 
 
-localPath = '~/autonomous/data'
+localPath = '/home/pi/autonomous/data/'
 
-command = 'scp -i /home/pi/.ssh/id_rsa -r localPath remotePath
+command = 'scp -i ~/.ssh/id_rsa -r ' + localPath + ' ' + remotePath
+
+#command = 'sudo scp -i ~/.ssh/id_rsa -r /home/pi/autonomous/data/ jim@jim-XPS-13-9360.local:/home/jim/autonomous/'
+
 
 call ( command, shell=True )
