46c50b5f4f0c5229e90e968d5a2cf23c6f7ce912
digsrus <jeo@jims-Mac-mini.local>
2018-03-11 21:25:42 -0400

    fubal

--- services/testSCP.py
+++ services/testSCP.py
@@ -7,10 +7,13 @@ from subprocess import call
 
 
 #   ---- Generate RSA keys: ----
-#   sudo ssh-keygen -t rsa
+#   ssh-keygen -t rsa
 
 #   ---- Copy public key to remote machine: ----
-#   sudo ssh-copy-id -i [path to rsa file] user@machine
+#   ssh-copy-id -i [path to rsa file] user@machine
+
+# I found the culprit. In System Preferences / Sharing / Remote Login, the access rights were checked for Administrator only. 
+# After I changed to Everyone, to worked like it used to.
 
 #remotePath = 'jim@jims-Mac-mini.local:/home/jim/autonomous/'
 remotePath = 'jim@jim-XPS-13-9360.local:/home/jim/autonomous/'
