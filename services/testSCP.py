2245822547fae54b2d8111af2f3a2eacb159df16
digsrus <jeo@jims-Mac-mini.local>
2018-03-13 22:18:47 -0400

    changes

--- services/testSCP.py
+++ services/testSCP.py
@@ -6,14 +6,18 @@ import subprocess
 from subprocess import call
 
 
-#   ---- Generate RSA keys: ----
+#   ---- On pi, Generate RSA keys: ----
 #   ssh-keygen -t rsa
 
-#   ---- Copy public key to remote machine: ----
-#   ssh-copy-id -i [path to rsa file] user@machine
+#    --- On mac, do this: ----
+# In System Preferences / Sharing / Remote Login, change access rights to all users 
 
-# I found the culprit. In System Preferences / Sharing / Remote Login, the access rights were checked for Administrator only. 
-# After I changed to Everyone, to worked like it used to.
+#  These needed to be changed too:
+# chmod 700 ~/.ssh
+# chmod 600 ~/.ssh/authorized_keys
+
+#   ---- From Pi, Copy public key to mac: ----
+#   pi@jimPi:~/.ssh $ ssh-copy-id jeo@jims-mac-mini.local
 
 #remotePath = 'jim@jims-Mac-mini.local:/home/jim/autonomous/'
 remotePath = 'jim@jim-XPS-13-9360.local:/home/jim/autonomous/'
