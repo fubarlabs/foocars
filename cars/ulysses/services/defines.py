a303c48680a09bab427b96b2892573ff80005249
Will Roscoe <wroscoe@gmail.com>
2018-04-18 11:38:02 -0400

    first version integrating Jenny's code

---
+++ cars/ulysses/services/defines.py
@@ -0,0 +1,53 @@
+import RPi.GPIO as GPIO
+
+LED_names={ 
+  "boot_RPi" : 2,
+  "shutdown_RPi" : 3,
+  "autonomous" : 4,
+  "collect_data" : 27,
+  "save_to_USBdrive" : 22,
+  "read_from_USBdrive" : 17,
+}
+
+
+switch_names={ 
+  "shutdown_RPi" : 9,
+  "autonomous" : 11,
+  "collect_data" : 6,
+  "save_to_USBdrive" : 13,
+  "read_from_USBdrive" : 5,
+}
+
+DATA_DIR = "/home/pi/foocars/cars/ulysses/data/"
+COLLECT_DIR = DATA_DIR + "collected/"
+WEIGHTS_DIR = DATA_DIR + "weights/"
+#WEIGHTS_FILE = WEIGHTS_DIR + "weights_2018-04-15_21-43-33_epoch_20.h5"
+WEIGHTS_FILE = WEIGHTS_DIR + "weights_2018-04-15_21-43-33_epoch_40.h5"
+STEERSTATS_FILE = WEIGHTS_DIR + "steerstats.npz"
+THR_MAX = 1590
+
+SWITCH_ON=GPIO.LOW
+SWITCH_OFF=GPIO.HIGH
+
+LED_ON=GPIO.HIGH
+LED_OFF=GPIO.LOW
+
+#function returns dynamically created object of type 'Enum' with fields enums:
+def enum(**enums):
+  return type('Enum', (), enums) 
+
+commandEnum=enum(
+  NOT_ACTUAL_COMMAND=0,
+  RC_SIGNAL_WAS_LOST=1, 
+  RC_SIGNALED_STOP_AUTONOMOUS=2, 
+  STEERING_VALUE_OUT_OF_RANGE=3, 
+  THROTTLE_VALUE_OUT_OF_RANGE=4, 
+  RUN_AUTONOMOUSLY=5, 
+  STOP_AUTONOMOUS=6, 
+  STOPPED_AUTO_COMMAND_RECIEVED=7, 
+  NO_COMMAND_AVAILABLE=8, 
+  GOOD_PI_COMMAND_RECEIVED=9, 
+  TOO_MANY_VALUES_IN_COMMAND=10, 
+  GOOD_RC_SIGNALS_RECIEVED=11)
+
+
