a07ade4f1ee9c56c8f57f78dccee8834d18e9966
ottopiuser <rick.rickanderson@gmail.com>
2018-05-01 21:35:38 -0400

    goes faster now

--- cars/motto/services/dropout_model.py
+++ cars/motto/services/dropout_model.py
@@ -25,49 +25,53 @@ frame_in = Input(shape=(3, nrows, ncols), name='img_input')
 
 #we should do a local contrast normalization
 
+print("adding first convolutional layer")
 #5x5 convolutional layer with a stride of 2
-model.add(BatchNormalization(input_shape=(nrows, ncols, 3)))
-model.add(Conv2D(24, (5, 5), activation='elu', padding='same', kernel_initializer='lecun_uniform'))
-model.add(MaxPooling2D(pool_size=(2, 2), data_format="channels_last"))
-model.add(Dropout(dp))
+#model.add(BatchNormalization(input_shape=(nrows, ncols, 3)))
+model.add(Conv2D(24, (5, 5), input_shape=(nrows, ncols, 3), strides=(2, 2), activation='elu', padding='same', kernel_initializer='lecun_uniform'))
+#model.add(MaxPooling2D(pool_size=(2, 2), data_format="channels_last"))
+#model.add(Dropout(dp))
 
+print("adding second convolutional layer")
 #5x5 convolutional layer with a stride of 2
-model.add(BatchNormalization())
-model.add(Conv2D(32, (5, 5), activation='elu', padding='same', kernel_initializer='lecun_uniform'))
-model.add(MaxPooling2D(pool_size=(2, 2), data_format="channels_last"))
-model.add(Dropout(dp))
+#model.add(BatchNormalization())
+model.add(Conv2D(32, (5, 5), strides=(2, 2), activation='elu', padding='same', kernel_initializer='lecun_uniform'))
+#model.add(MaxPooling2D(pool_size=(2, 2), data_format="channels_last"))
+#model.add(Dropout(dp))
 
-
+print("adding third convolutional layer")
 #5x5 convolutional layer with a stride of 2
-model.add(BatchNormalization())
-model.add(Conv2D(40, (5, 5), activation='elu', padding='same', kernel_initializer='lecun_uniform'))
-model.add(MaxPooling2D(pool_size=(2, 2), data_format="channels_last"))
-model.add(Dropout(dp))
+#model.add(BatchNormalization())
+model.add(Conv2D(40, (5, 5), strides=(2, 2), activation='elu', padding='same', kernel_initializer='lecun_uniform'))
+#model.add(MaxPooling2D(pool_size=(2, 2), data_format="channels_last"))
+#model.add(Dropout(dp))
 
-
+print("adding fourth convolutional layer")
 #3x3 convolutional layer with no stride 
-model.add(BatchNormalization())
-model.add(Conv2D(48, (3, 3), activation='elu', padding='same', kernel_initializer='lecun_uniform'))
-model.add(MaxPooling2D(pool_size=(2, 2), data_format="channels_last"))
-model.add(Dropout(dp))
+#model.add(BatchNormalization())
+model.add(Conv2D(48, (3, 3), strides=(2, 2), activation='elu', padding='same', kernel_initializer='lecun_uniform'))
+#model.add(MaxPooling2D(pool_size=(2, 2), data_format="channels_last"))
+#model.add(Dropout(dp))
 
-
+print("adding fifth convolutional layer")
 #3x3 convolutional layer with no stride 
-model.add(BatchNormalization())
-model.add(Conv2D(48, (3, 3), activation='elu', padding='same', kernel_initializer='lecun_uniform'))
-model.add(MaxPooling2D(pool_size=(2, 2), data_format="channels_last"))
-model.add(BatchNormalization())
-model.add(Dropout(dp))
+#model.add(BatchNormalization())
+model.add(Conv2D(48, (3, 3), strides=(2, 2), activation='elu', padding='same', kernel_initializer='lecun_uniform'))
+#model.add(MaxPooling2D(pool_size=(2, 2), data_format="channels_last"))
+#model.add(BatchNormalization())
+#model.add(Dropout(dp))
 
 
 model.add(Flatten())
 
+print("adding fully connected layer")
 #fully connected layer
 model.add(Dense(100, activation='elu', kernel_initializer='lecun_uniform'))
 #model.add(Dropout(dp))
 
+print("adding output layer")
 #fully connected layer to output node
 model.add(Dense(1, activation='linear', kernel_initializer='lecun_uniform'))
 
 model.compile(loss=['mse'], optimizer=SGD(lr=0.001, decay=1e-6, momentum=0.9, nesterov=True), metrics=['mse'])
-print(model)
+print(model.summary())
