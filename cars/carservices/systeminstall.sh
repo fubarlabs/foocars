924a1b75adb5b18c1694bca6da0a6f9310f667cb
Rick Anderson <rick.rickanderson@gmail.com>
2021-04-26 14:51:48 -0400

    Update systeminstall.sh

--- cars/carservices/systeminstall.sh
+++ cars/carservices/systeminstall.sh
@@ -1,10 +1,1 @@
-sudo apt-get  install build-essential cmake pkg-config
-\ libjpeg-dev libtiff5-dev libjasper-dev libpng-dev
-\ libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
-\ libxvidcore-dev libx264-dev
-\ libfontconfig1-dev libcairo2-dev
-\ libgdk-pixbuf2.0-dev libpango1.0-dev
-\ libgtk2.0-dev libgtk-3-dev
-\ libatlas-base-dev gfortran
-\ libhdf5-dev libhdf5-serial-dev libhdf5-103
-\ libqtgui4 libqtwebkit4 libqt4-test python3-pyqt5
+sudo apt-get  install build-essential cmake pkg-config libjpeg-dev libtiff5-dev libjasper-dev libpng-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libfontconfig1-dev libcairo2-dev libgdk-pixbuf2.0-dev libpango1.0-dev libgtk2.0-dev libgtk-3-dev libatlas-base-dev gfortran libhdf5-dev libhdf5-serial-dev libhdf5-103 libqtgui4 libqtwebkit4 libqt4-test python3-pyqt5
